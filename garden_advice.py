aec60abc2a468a2332812ee13c7bb92b8a5a3b13
Setjhaba Motsoahole <Setjhaba_M@icloud.com>
2026-01-02 10:48:03 +0200

    Added detailed comments

--- garden_advice.py
+++ garden_advice.py
@@ -1,13 +1,16 @@
-# Hardcoded values for the season and plant type
+# Season variable that takes in user input
+# and converts it to lower case.
 season = input("Enter the season that you are in: ").lower()
 # TODO: # Replace with input() to allow user interaction.
+# Plant_type variable that takes in user input
+# and converts it to lowercase.
 plant_type = input("Enter the plant type: ").lower()
 # TODO: Replace with input() to allow user interaction.
 
 # Variable to hold gardening advice
 advice = ""
 
-# Determine advice based on the season
+# Determine advice based on user input of  the season
 if season == "summer":
     advice += "Water your plants regularly and provide some shade.\n"
 elif season == "winter":
@@ -15,7 +18,7 @@ elif season == "winter":
 else:
     advice += "No advice for this season.\n"
 
-# Determine advice based on the plant type
+# Determine advice based on user input of the plant type
 if plant_type == "flower":
     advice += "Use fertiliser to encourage blooms."
 elif plant_type == "vegetable":
